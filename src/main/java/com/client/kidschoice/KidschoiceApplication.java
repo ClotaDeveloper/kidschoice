b2505d4ab235a87c5e7fedd17123dcb5eb545e19
ClotaDeveloper <158062682+ClotaDeveloper@users.noreply.github.com>
2025-04-14 15:14:14 +0530

    First Comment

---
+++ src/main/java/com/client/kidschoice/KidschoiceApplication.java
@@ -0,0 +1,13 @@
+package com.client.kidschoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidschoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidschoiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/client/kidschoice/KidschoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.client.kidschoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidschoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
